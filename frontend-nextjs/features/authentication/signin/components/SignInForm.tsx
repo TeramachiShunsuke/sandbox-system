f53897ba957cbb2fc3af99fd5140dfec7a21a65d
TeramachiShunsuke <like.football.17@gmail.com>
2024-09-01 00:29:16 +0900

    designのしゅうせいおよびKubenetesの作成

--- frontend-nextjs/features/authentication/signin/components/SignInForm.tsx
+++ frontend-nextjs/features/authentication/signin/components/SignInForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button, Box, Card, Flex, TextField } from '@radix-ui/themes';
+import { Button, Box, Card, Flex, TextField, Heading, Text } from '@radix-ui/themes';
 import { ChangeEvent } from 'react';
 import { useSignin } from '../hooks/useSignin';
 
@@ -12,40 +12,61 @@ const SignInForm = () => {
     handleSubmit,
     loading,
   } = useSignin();
+
   return (
-    <Box maxWidth="400px">
-      <Card>
-        <h2>SignIn</h2>
-        <Flex gap="3" align="center">
-          <form onSubmit={handleSubmit} className="p-2 m-4">
-            <div>
-              <TextField.Root
-                placeholder="username"
-                value={username}
-                type="text"
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setUsername(e.target.value)
-                }
-              />
-            </div>
-            <div className='py-2'>
-              <TextField.Root
-                placeholder="password"
-                value={password}
-                type="password"
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setPassword(e.target.value)
-                }
-              />
-            </div>
-            <div>
-              {loading && <p>Loading...</p>}
-              <Button type="submit">Sign In</Button>
-            </div>
+    <Flex minHeight="100%" align="center" justify="center">
+      <Box width="100%" maxWidth="400px" p="4" mx="auto">
+        <Card>
+          <Heading as="h2" size="8" align="center" mb="4">
+            Sign In to Your Account
+          </Heading>
+          <form onSubmit={handleSubmit}>
+            <Flex direction="column" gap="4">
+              <div>
+                <label htmlFor="username">Username</label>
+                <TextField.Root
+                  id="username"
+                  placeholder="usesrname"
+                  value={username}
+                  type="text"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setUsername(e.target.value)
+                  }
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <TextField.Root
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  type="password"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setPassword(e.target.value)
+                  }
+                  required
+                />
+              </div>
+              <Flex justify="between" align="center">
+                <Text size="4" color="ruby">
+                  <input type="checkbox" id="remember-me" className="mr-2" />
+                  <label htmlFor="remember-me">Remember me</label>
+                </Text>
+                <Text size="4">
+                  <a href="#" className="text-primary hover:text-primary-foreground">
+                    Forgot your password?
+                  </a>
+                </Text>
+              </Flex>
+              <Button type="submit" disabled={loading} color="ruby" size="4">
+                {loading ? 'Signing in...' : 'Sign In'}
+              </Button>
+            </Flex>
           </form>
-        </Flex>
-      </Card>
-    </Box>
+        </Card>
+      </Box>
+    </Flex>
   );
 };
 
---
+++ minikube/README.md
@@ -0,0 +1,33 @@
+# 
+
+
+```sh
+cd minikube
+docker login # DockerHubのパブリックなものを取得してくるため
+kubectl apply -f <ここにあるyamlすべてアプライしてください。>
+# ingreessの有効化
+minikube addons enable ingress
+kubectl apply -f ingress.yaml
+kubectl apply -f ingress-service.yaml
+
+```
+```sh
+vim /etc/hosts
+# 以下を追加
+# 127.0.0.1 frontend.minikube.local
+```
+
+[デプロイ先](http://frontend.minikube.local)
+
+
+成功している時のアクセスの仕方
+```sh
+# frontend pod へのアクセス
+kubectl port-forward svc/frontend 3000:3000
+
+# backend pod へのアクセス
+kubectl port-forward svc/backend  9000:9000
+
+# database pod へのアクセス
+kubectl port-forward svc/db  3306:3306
+```
